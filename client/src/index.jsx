059172c8fd5933bc866c247cab697657e91b0c0a
Kevin Pugliese <kevin.lima.contato@gmail.com>
2018-02-09 10:13:44 -0200

    temp

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './main/app';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import promise from 'redux-promise';
 
+import App from './main/app';
+import reducers from './main/reducers';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = applyMiddleware(promise)(createStore)(reducers);
 ReactDOM.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
   , document.getElementById('app'));
